6cc986a4bd167063adf0e72cdd19994612d88123
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-14 22:56:12 +0100

    Updated navabr.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,16 +5,17 @@ import logo from '../assets/logo.svg'
 
 const Navbar = () => {
   return (
-    <nav className='w-full absolute z-10 top-0 left-0'> 
+    <nav className='w-full absolute z-10 top-0 left-0 bg-cover bg-center ' style={{backgroundImage: "url('src/assets/header_img.png')"}}> 
     
-    <div className=''> 
-      <img src={logo} alt="Logo" />
-         <div>
+    <div className=' flex justify-between bg-transparent py-4 px-6 md:px-16 mx-auto lg:px-32'> 
+    <img src={logo} alt="Logo"/>
+         <div className='gap-8 hidden md:flex text-white font-medium items-center'>
             <a href="#header" className='cursor-pointer hover:text-gray-400'>Home</a>
             <a href="#about" className='cursor-pointer hover:text-gray-400'>About</a>
             <a href="#project" className='cursor-pointer hover:text-gray-400'>Projects</a>
             <a href="#testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
          </div>
+         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
     </div>
 
     </nav>
