980b49cdf044d8ef85f76bb40fbc8b828f794886
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-14 18:44:22 +0100

    Added anchor tags to a div

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,10 +6,15 @@ import logo from '../assets/logo.svg'
 const Navbar = () => {
   return (
     <nav className='w-full absolute z-10 top-0 left-0'> 
-
+    
     <div className=''> 
       <img src={logo} alt="Logo" />
-
+         <div>
+            <a href="#header" className='cursor-pointer hover:text-gray-400'>Home</a>
+            <a href="#about" className='cursor-pointer hover:text-gray-400'>About</a>
+            <a href="#project" className='cursor-pointer hover:text-gray-400'>Projects</a>
+            <a href="#testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+         </div>
     </div>
 
     </nav>
