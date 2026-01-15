d6656f8887daeef8d9f130751cbfcf3f0bf8363a
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-15 17:33:13 +0100

    Updated div style for header section

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
-
+import menu from '../assets/menu_icon.svg'
 
 
 const Navbar = () => {
@@ -16,7 +16,14 @@ const Navbar = () => {
             <a href="#testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
          </div>
          <button className='hidden md:block bg-white font-semibold px-8 py-2 rounded-full'>Sign Up</button>
+         <img src={menu} alt="menu" className='md:hidden w-7 cursor-pointer'/>
+
     </div>
+
+        {/*--------MOBILE MENU--*/}
+
+        
+
 
     </nav>
   )
