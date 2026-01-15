f56478480074ecaa30144ccc5cf38bccf5b98cbc
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-15 14:51:04 +0100

    Changed the button tags to anchor tags

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,12 +6,16 @@ const Header = () => {
     <div className='w-full h-screen bg-cover bg-center' style={{backgroundImage: "url('src/assets/header_img.png')"}} id="header">
        <Navbar /> 
        <div>
-        <h1 className="text-white text-6xl md:text-6xl font-bold ">Welcome to Our Website  Explore </h1>
-        <button>Projects</button>
-        <button>Contact Us </button>
+        <h1 className="text-white text-6xl md:text-6xl font-bold "> Explore home that fits your Dream. </h1>
+
+        <div>
+        <a href="#projects"><button>Projects</button></a>
+        <a href="#contact"><button>Contact Us </button></a>
+        </div>
+       
        </div>
     </div>
   )
-}
+};
 
 export default Header
