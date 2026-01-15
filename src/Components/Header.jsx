dc01e4281f11badc22a3ffb7e7cbfc65259fb88b
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-15 16:50:04 +0100

    Updated div style for header section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,12 +5,12 @@ const Header = () => {
   return (
     <div className='w-full min-h-screen mb-4 flex items-center bg-cover bg-center overflow-hidden ' style={{backgroundImage: "url('src/assets/header_img.png')"}} id="header">
        <Navbar /> 
-       <div className='mx-auto py-4 px-6 md:px-16 lg:px-32 text-white'>
-        <h1 className=" text-5xl sm:text-6xl pt-16 md:text-(--82px) max-w-3xl inline-block font-bold "> Explore home that fits your Dream. </h1>
+       <div className='mx-auto py-4 px-6 md:px-16 lg:px-32 text-white text-center'>
+        <h1 className=" text-5xl sm:text-6xl pt-20 md:text-(--82px) max-w-3xl inline-block font-bold "> Explore home that fits your Dream. </h1>
 
-        <div>
-        <a href="#projects" className=''><button>Projects</button></a>
-        <a href="#contact" className='py-2 px-6'><button>Contact Us </button></a>
+        <div className='space-x-6 mt-16'>
+        <a href="#projects" className='rounded border border-white py-3 px-8'><button>Projects</button></a>
+        <a href="#contact" className='py-3 px-8 border border-white rounded'><button>Contact Us </button></a>
         </div>
        
        </div>
