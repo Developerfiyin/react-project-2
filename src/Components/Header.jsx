1ad15b1484dce83aead4c787903fd4568ce1e9a9
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-15 16:30:33 +0100

    Updated Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,14 +3,14 @@ import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <div className='w-full h-screen bg-cover bg-center' style={{backgroundImage: "url('src/assets/header_img.png')"}} id="header">
+    <div className='w-full min-h-screen mb-4 flex items-center bg-cover bg-center overflow-hidden ' style={{backgroundImage: "url('src/assets/header_img.png')"}} id="header">
        <Navbar /> 
-       <div>
-        <h1 className="text-white text-6xl md:text-6xl font-bold "> Explore home that fits your Dream. </h1>
+       <div className='mx-auto py-4 px-6 md:px-16 lg:px-32 text-white'>
+        <h1 className=" text-5xl sm:text-6xl pt-16 md:text-(--82px) max-w-3xl inline-block font-bold "> Explore home that fits your Dream. </h1>
 
         <div>
-        <a href="#projects"><button>Projects</button></a>
-        <a href="#contact"><button>Contact Us </button></a>
+        <a href="#projects" className=''><button>Projects</button></a>
+        <a href="#contact" className='py-2 px-6'><button>Contact Us </button></a>
         </div>
        
        </div>
