4d1d8be6bf4d34dc8c8d88d7b56f4640f5834697
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-16 21:24:43 +0100

    Updated Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import logo from '../assets/logo.svg'
 import menu from '../assets/menu_icon.svg'
 import cross from '../assets/cross_icon.svg'
+import { useState } from 'react'
 
 
 
 const Navbar = () => {
-    const [menuOpen, setMenuOpen] = React.useState(false);
-
+    const [menuOpen, setMenuOpen] = useState(false);
+         
+         useEffect(() => {
+        if(menuOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    })
+   
   return (
     <nav className='w-full absolute z-10 top-0 left-0 '> 
     
@@ -20,15 +29,15 @@ const Navbar = () => {
             <a href="#testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
          </div>
          <button className='hidden md:block bg-white font-semibold px-8 py-2 rounded-full'>Sign Up</button>
-         <img src={menu} alt="menu" className='md:hidden w-7 cursor-pointer'/>
+         <img onClick={ () => setMenuOpen (true)} src={menu} alt="menu icon" className='md:hidden w-7 cursor-pointer'/>
 
     </div>
 
         {/*--------MOBILE MENU--*/}
 
-       <div className='md:hidden items-center fixed top-0 bottom-0 right-0 overflow-hidden bg-white transition-all '> 
+       <div className={`'md:hidden  items-center ${menuOpen ? 'fixed w-full' : 'h-0 w-0'} fixed top-0 w-full bottom-0 right-0 overflow-hidden bg-white transition-all '`}> 
         <div className='flex justify-end p-6 cursor-pointer'>
-            <img src={cross} alt="THE CROSS ICON" className=' w-6 ' />
+            <img onClick={ () => setMenuOpen (false)} src={cross} alt="THE CROSS ICON" className=' w-6' />
         </div>
 
         <div className='flex space-x-5 flex-col items-center gap-3 mt-5 text-lg font-medium transition-all duration-300 '> 
