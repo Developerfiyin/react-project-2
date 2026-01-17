2ada006f230a0dcd27eb26da81638f3b488521a5
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-17 15:57:27 +0100

    inserted an image inside a div

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import image from '../assets/brand_img.png'
 const About = () => {
   return (
     <main className='mx-auto flex bg-white flex-col container text-center items-center justify-center px-14 py-4 md:px-16 lg:32 mt-4 w-full overflow-hidden ' id="about">
@@ -8,8 +8,14 @@ const About = () => {
         <h2 className='text-3xl font-bold mb-2 sm:text-4xl '> About Us <span className='underline underline-offset-4 decoration-1 font-light ' > Our Brands</span> </h2>
         <p className='text-gray-500 max-w-80 mb-8 text-center'> Passionate about properties Dedicated to our vision.</p>
 
-        <div className=''>
+        <div className=' flex flex-col md:flex-row items-center md:items-start md:gap-20 '>
 
+          <img src={image} alt="Our Brands  "  className='w-full sm:w1/2 max-w-lg '/>
+
+
+<div>
+  
+</div>
         </div>
         </div>
 
