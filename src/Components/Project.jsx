153095057fb006cd69b4cc1bdc26d32bc90e6f83
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-17 23:29:09 +0100

    added an h1 tag

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -4,7 +4,12 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div>Project</div>
+    <main className=' container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 w-full overflow-hidden ' id="project" >
+       <h1 className='font-bold sm:text-4xl text-2xl mb-2 text-center  '>Projects <span className='underline underline-offset-4 decoration-1 font-light'>Completed</span> </h1>
+          <p className='text-gray-500 text-center mb-8 max-w-80 mx-auto'> Crafting spacing, Building Legacies-Explore Our Portfolio</p>
+              
+
+        </main>
   )
 }
 
