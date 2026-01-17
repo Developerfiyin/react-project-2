0b281cf325876833153b4ebf0039fd39d3cf708f
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-17 21:32:35 +0100

    Added a button of the text learn more to the div

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -37,6 +37,7 @@ const About = () => {
            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
          </p>
+         <button className='bg-blue-500 text-white px-6 py-2 rounded-md hover:bg-blue-800 transition duration-300'> Learn More</button>
        </div>
         
         </div>
