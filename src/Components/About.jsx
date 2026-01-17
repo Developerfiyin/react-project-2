81ef62d8f532a2c940a4244ae52e6143ebcb45b3
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-17 15:47:09 +0100

    Updated about.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <main className='mx-auto bg-white flex-col text-center items-center justify-center px-14 py-4 md:px-16 lg:32 mt-4 w-full overflow-hidden ' id="about">
+    <main className='mx-auto flex bg-white flex-col container text-center items-center justify-center px-14 py-4 md:px-16 lg:32 mt-4 w-full overflow-hidden ' id="about">
         
         <div>
-        <h2 className='text-3xl font-bold mb-2 sm:text-4xl '> About Us <span className='underline underline-offset-4 decoration-1 font-light ' > Our brands</span> </h2>
+        <h2 className='text-3xl font-bold mb-2 sm:text-4xl '> About Us <span className='underline underline-offset-4 decoration-1 font-light ' > Our Brands</span> </h2>
+        <p className='text-gray-500 max-w-80 mb-8 text-center'> Passionate about properties Dedicated to our vision.</p>
+
+        <div className=''>
+
+        </div>
         </div>
 
-
+                                                        
     </main>
   )
 }
