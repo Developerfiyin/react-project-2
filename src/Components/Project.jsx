f9f92eeeba3a697c4b6d5f41649d94f4dff5eee8
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-18 03:27:50 +0100

    Added images into button tag

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import sliderLeft from '../assets/sliderLeft.png'
+import sliderRight from '../assets/sliderRight.png'
 
 
 const Project = () => {
@@ -8,6 +9,14 @@ const Project = () => {
        <h1 className='font-bold sm:text-4xl text-2xl mb-2 text-center  '>Projects <span className='underline underline-offset-4 decoration-1 font-light'>Completed</span> </h1>
           <p className='text-gray-500 text-center mb-8 max-w-80 mx-auto'> Crafting spacing, Building Legacies-Explore Our Portfolio</p>
               
+              {/* Slider buttons */}
+              <button>
+                <img src={sliderLeft} alt="Left image"  />
+              </button>
+
+              <button>
+                <img src={sliderRight} alt="Right image" />
+              </button> 
 
         </main>
   )
