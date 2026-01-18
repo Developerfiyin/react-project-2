d067f0c81758a59dee30eebf7a0f1722d736a683
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-18 23:38:40 +0100

    Added comment to some codes

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -2,17 +2,31 @@ import React from 'react'
 import sliderLeft from '../assets/left_arrow.svg'
 import sliderRight from '../assets/right_arrow.svg'
 import {assets, projectsData } from '../assets/assets'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 
 const Project = () => {
-     
     
-    const [currentIndex, setCurrentIndex] = React.useState(0);  
-    const [itemsToShow, setItemsToShow] = React.useState(1);
+    const [currentIndex, setCurrentIndex] = useState(0);  
+    const [itemsToShow, setItemsToShow] = useState(1);
 
-    
-        
+  /*  useEffect(() => {
+        const updateItemsToShow = () => {
+            const screenWidth = window.innerWidth;
+            if (screenWidth < 640) {
+                setItemsToShow(1);
+            } else if (screenWidth < 1024) {
+                setItemsToShow(2);
+            } else {
+                setItemsToShow(3);
+            }
+        };
+
+        updateItemsToShow();
+        window.addEventListener('resize', updateItemsToShow);
+
+        return () => window.removeEventListener('resize', updateItemsToShow);
+    }, []); */
 
     const nextProject = () => {
         setCurrentIndex((prevIndex) =>
@@ -21,7 +35,7 @@ const Project = () => {
     };
 
     const prevProject = () => {
-        setCurrentIndex((prevIndex) =>
+        setCurrentIndex((prevIndex) =>    
         (prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1)
         );
     }; 
@@ -31,18 +45,18 @@ const Project = () => {
        <h1 className='font-bold sm:text-4xl text-2xl mb-2 text-center  '>Projects <span className='underline underline-offset-4 decoration-1 font-light'>Completed</span> </h1>
           <p className='text-gray-500 text-center mb-8 max-w-80 mx-auto'> Crafting spacing, Building Legacies-Explore Our Portfolio</p>
               
+              {/*Slider buttons */ }
               <div className='flex justify-end items-center mb-8 '>
-                <button className='p-3 bg-gray-200 rounded mr-2' onClick={prevProject}>
-                  <img src={sliderLeft} alt="Left image" className='' aria-label='Previous project'  />
+                <button className='p-3 bg-gray-200 rounded mr-2' onClick={prevProject} aria-label='Previous Project'>
+                  <img src={sliderLeft} alt="Left image" />
                 </button>
 
-              <button className='p-3 bg-gray-200 rounded mr-2' onClick={nextProject}>
-                <img src={sliderRight} alt="Right image" className='' aria-label='Next project' />
+              <button className='p-3 bg-gray-200 rounded mr-2' onClick={nextProject} aria-label='Next Project'>
+                <img src={sliderRight} alt="Right image" />
               </button> 
                 </div>
 
             {/*-----PROJECT CARDS----*/}
-
             <div className='overflow-hidden '>
                 <div className='flex gap-8 transition-transform duration-500 ease-in-out' style={{transform: `translateX(-${currentIndex * (100 / itemsToShow)}%)` }}>
                    {projectsData.map((project, index ) => (
@@ -51,8 +65,7 @@ const Project = () => {
                       <div className='absolute left-0 right-0 bottom-5 flex justify-center '>
                         <div className='bg-white inline-block w-3/4 px-4 py-2  shadow-md'>
                         <h2 className='text-xl font-semibold mb-2 text-gray-800'> {project.title} </h2>
-                        <p className='text-gray-600 text-sm'> {project.price} <span>  </span>   {project.location} </p>
-
+                        <p className='text-gray-600 text-sm'> {project.price} <span> | </span>   {project.location} </p>
 
                         </div>
 
