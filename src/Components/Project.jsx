6bdec0ae099da8d63ad5cce9945f3eab4a2faeb9
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-18 16:14:16 +0100

    Updated project.jsx

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -5,6 +5,22 @@ import {assets, projectsData } from '../assets/assets'
 
 
 const Project = () => {
+
+    const [currentIndex, setCurrentIndex] = React.useState(0);  
+    const [itemsToShow, setItemsToShow] = React.useState(1);
+
+    const nextProject = () => {
+        if (currentIndex < projectsData.length - itemsToShow) {
+            setCurrentIndex(currentIndex + 1);
+        }
+    };
+    
+    const prevProject = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        }       
+    };
+
   return (
     <main className=' container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 w-full overflow-hidden ' id="project" >
        <h1 className='font-bold sm:text-4xl text-2xl mb-2 text-center  '>Projects <span className='underline underline-offset-4 decoration-1 font-light'>Completed</span> </h1>
@@ -24,18 +40,19 @@ const Project = () => {
 
             <div className='overflow-hidden '>
                 <div className='flex gap-8 transition-transform duration-500 ease-in-out '>
-                   {projectsData.map( (project, index ) => (
-                    <div key={index} className='mb-10 relative shrink w-full sm:w-1/4'>
-                      <img src={project.image} alt={project.title} className='w-full h-auto rounded-lg mb-4 '/>     
+                   {projectsData.map((project, index ) => (
+                    <div key={index} className='relative shrink-0 w-full sm:w-1/4'>
+                      <img src={project.image} alt={project.title} className='w-full h-auto mb-14' />     
                       <div className='absolute left-0 right-0 bottom-5 flex justify-center '>
                         <div className='bg-white inline-block w-3/4 px-4 py-2  shadow-md'>
+                        <h2 className='text-xl font-semibold mb-2 text-gray-800'> {project.title} </h2>
+                        <p className='text-gray-600 text-sm'> {project.price} <span></span>   {project.location} </p>
 
 
                         </div>
 
                       </div>
-                        <h2 className='text-xl font-semibold mb-2'> {project.title} </h2>
-                        <p className='text-gray-600'> {project .description} </p>
+
                     </div>
                      ))}
 
