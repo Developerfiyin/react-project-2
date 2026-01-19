bed5c3c69a9848df7846f6a0dc805f1bc8666edb
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-19 12:16:19 +0100

    Added p tag to a div

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const Testimonials = () => {
   return (
     <main className='container mx-auto py-10 lg:px-32 w-full overflow-hidden '>
-
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center '> Customer <span className='underline underline-offset-4 decoration-1 font-light '> Testimonials</span>  </h1>
+
+        <p className='text-center text-gray-500 mb-12 max-w-80'> 
+            Real Stories from Those Who Found Home with Us.
+            </p>
       <div >
 
       </div>
