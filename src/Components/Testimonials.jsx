a94f32cd5f7a5b08f3778ada2f6d7adcd04d2f71
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-19 12:07:07 +0100

    Updated Testimonials file

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Testimonials = () => {
   return (
-    <main>
-        
-        Testimonials</main>
+    <main className='container mx-auto py-10 lg:px-32 w-full overflow-hidden '>
+
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center '> Customer <span className='underline underline-offset-4 decoration-1 font-light '> Testimonials</span>  </h1>
+      <div >
+
+      </div>
+
+        </main>
   )
 }
 
