ae4032c683ea42aacaf065fe9f1e41456aa9f4a7
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-19 21:59:36 +0100

    Updated contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <main className='py-12 '>
+    <main className='py-20 text-center p-6 w-full overflow-hidden lg:px-32 '>
         
-        Contact
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center '> Contact <span className='underline underline-offset-4 decoration-1 font-light'> With Us</span> </h1>    
+        <p> Ready to Make a Move? Let's Build Your  Future Together. </p>
+
+
+
         
         </main>
   )
