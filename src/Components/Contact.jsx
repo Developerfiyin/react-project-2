b3df1e93a4bd064b61deda3b1c62c1ca5b898565
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-19 22:59:49 +0100

    Added a form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,9 +7,23 @@ const Contact = () => {
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center '> Contact <span className='underline underline-offset-4 decoration-1 font-light'> With Us</span> </h1>    
         <p> Ready to Make a Move? Let's Build Your  Future Together. </p>
 
+    <form action="">
+    <div>
+            <div className='w-full md:w-1/2  text-left '>
+                        Your Name
+              <input type="text" placeholder='Your Name' name='Name' required 
+               className='w-full border border-gray-300 rounded py-3 px-4 mt-2 '  />
+            </div>
 
+           <div className='w-full md:w-1/2 text-left md:pl-4 '>
+            Your Email
+           </div>
 
-        
+    </div>
+
+</form>
+
+                                   
         </main>
   )
 }
