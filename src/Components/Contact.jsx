8de24c135ca8c79bfe620a75cd39fae3fef8a69a
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-20 17:54:18 +0100

    updated contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -19,11 +19,13 @@ const Contact = () => {
     const data = await response.json();
 
     if (data.success) {
-      setResult("Form Submitted Successfully");
+      setResult("");
+      alert("Form Submitted Successfully")
       event.target.reset();
     } else {
       console.log("Error", data);
-      setResult(data.message);
+      setResult("")
+      alert(data.message);
     }
 }
 
@@ -34,31 +36,29 @@ const Contact = () => {
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center '> Contact <span className='underline underline-offset-4 decoration-1 font-light'> With Us</span> </h1>    
         <p> Ready to Make a Move? Let's Build Your  Future Together. </p>
 
-    <form action="" className='max-w-2xl mx-auto text-gray-300 rounded py-3 '>
+    <form action="" onSubmit={onSubmit} className='max-w-2xl mx-auto text-gray-300 rounded py-3 '>
     <div className='flex flex-wrap '>
-            <div className='w-full md:w-1/2  text-left '>
+            <div className='w-full md:w-1/2  text-left'>
                         Your Name
               <input type="text" placeholder='Your Name' name='Name' required 
-               className='w-full border border-gray-300 rounded py-3 px-4 mt-2 '  />
+               className='w-full border border-gray-400 rounded py-3 px-4 mt-2 '  />
             </div>
 
            <div className='w-full md:w-1/2 text-left md:pl-4 '>
                Your Email
-            <input type="email" name='Email' required
-             className='w-full border border-gray-300 rounded py-3 px-4 mt-2 ' />
+            <input type="email" name='Email' required placeholder='Your Email'
+             className='w-full border border-gray-400 rounded py-3 px-4 mt-2 ' />
            </div>
      </div>
 
     <div> 
-        <textarea className='resize-none w-full pr-3 py-6 px-4 border rounded border-gray-300 mt-3'
-         name="text" id="" required > Message</textarea>
-    </div>
+        <textarea className='resize-none w-full pr-2 py-6 px-4 border rounded border-gray-400 mt-3'
+         name="text" required placeholder='Message'>Message</textarea>
+       </div>
     
-          <button className='bg-blue-500 rounded py-2 px-12'> bg-blue-500 </button> 
-
+          <button className='bg-blue-500 rounded py-2 px-12 mb-12'> Sumbit Form </button> 
 </form>
 
-                                   
         </main>
   )
 }
