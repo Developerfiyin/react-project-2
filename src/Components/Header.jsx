e2be235abdf474d5edadfa7df9e83609a9f5ad86
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-21 23:54:36 +0100

    updated motion

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { motion } from "motion/react"
+import { MotionConfig } from 'framer-motion'
 
 const Header = () => {
   return (
     <div className='w-full min-h-screen mb-4 flex items-center bg-cover bg-center overflow-hidden ' style={{backgroundImage: "url('src/assets/header_img.png')"}} id="header">
        <Navbar /> 
-       <div className='mx-auto py-4 px-6 md:px-20 lg:px-32 text-white text-center'>
+       <motion.div className='mx-auto py-4 px-6 md:px-20 lg:px-32 text-white text-center'>
         <h1 className=" text-5xl sm:text-6xl pt-20 md:text-(--82px) max-w-3xl inline-block font-bold "> Explore home that fits your Dream. </h1>
 
         <div className='space-x-6 mt-16'>
