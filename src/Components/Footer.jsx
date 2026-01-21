d1e7e335df6f5eaee3be88239fdee4061021e810
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-21 23:39:47 +0100

    updated footer section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
              </div>
        
      <div className='w-full md:w-1/5 mb-8 md:mb-0'> 
-         <h3 className='text-xl text-gray-900 font-bold mb-4'>Company </h3>
+         <h3 className='text-xl text-white font-bold mb-4'>Company </h3>
          <div className="flex flex-col gap-2 text-gray-400 ">
             <a href="#home" className='hover:text-white '>Home</a>
             <a href="about" className='hover:text-white'>About Us</a>
@@ -37,7 +37,10 @@ const Footer = () => {
     <div className='text-center border-t border-gray-800 mt-10 text-gray-400 '>
      Copyright 2024 © GreatStack. All Right Reserved. Owned by greatstack, recreated by me.
 </div>
-        
+
+
+
+
     </main>
   )
 }
