9f74f751c42ce7e18afc84027d671bffad0cb598
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-21 13:25:26 +0100

    Updated footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,16 +3,15 @@ import logo from '../assets/logo_dark.svg'
 
 const Footer = () => {
   return (
-    <main className='pt-10 '>
+    <main className='pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden '>
 
-
-    <div>  
+    <div className='flex flex-col mx-auto justify-between items-start md:flex-row  '>  
      <div>
-        <img src={logo} alt="Thee logo " className=''  />
+        <img src={logo} alt="The dark logo image " className=''  />
         <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text. </p>
-
-          </div>
+             Lorem Ipsum has been the industry's standard dummy text. </p> 
+             </div>
+             
      <div>  </div>
      <div>  </div>
 
