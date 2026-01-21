5c41c4b8d886d5abf925277bcd7b6aa249ea9511
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-21 13:16:26 +0100

    Updated footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import logo from '../assets/logo_dark.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <main className='pt-10 '>
+
+
+    <div>  
+     <div>
+        <img src={logo} alt="Thee logo " className=''  />
+        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+             Lorem Ipsum has been the industry's standard dummy text. </p>
+
+          </div>
+     <div>  </div>
+     <div>  </div>
+
+    </div>
+
+ {/* second div*/}
+<div>
+
+
+</div>
+        
+    </main>
   )
 }
 
