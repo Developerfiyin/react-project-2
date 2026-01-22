d95552150623f2109c5219e9ad2eedf244253c4d
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-22 10:09:26 +0100

    Added animation to the header file

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
        initial={{opacity: 0, y:100 }}
        transition={{duration : 1.5}}
         whileInView={ {opacity:1, y:0}}
+         viewport={{once: true}}
 
-        
        className='mx-auto py-4 px-6 md:px-20 lg:px-32 text-white text-center'>
         <h1 className=" text-5xl sm:text-6xl pt-20 md:text-(--82px) max-w-3xl inline-block font-bold "> Explore home that fits your Dream. </h1>
 
